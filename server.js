687bb66d3adb63709b61823f621def493ad66c16
Mansi Ganar <mansi99ganar@gmail.com>
2024-04-20 14:00:47 +0200

    bufferring options added

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const fs = require("fs");
 require("dotenv").config();
-import "./jsondata.json";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,6 +15,7 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose
   .connect(`${dbURL}`, {
+    bufferCommands: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
